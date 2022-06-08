2d6c925f6c7af5b40fc94a331c484a5b75a9174a
Nayan P Ingale <97461960+NayanIngale1@users.noreply.github.com>
2022-06-08 20:55:13 +0530

    update: delete not work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
+import "./App.css";
+import TaskApp from "./components/TaskApp";
+import jsonData from "./data/tasks.json";
+
 function App() {
-  return <div>{/* Code Here */}</div>;
+  return (
+    <div>
+      {/* Code Here */}
+      <TaskApp jsonData={jsonData} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./addTask.module.css";
 
-const AddTask = () => {
+const AddTask = ({ handleAddTask }) => {
   // NOTE: do not delete `data-testid` key value pair
+  const [task, setTask] = useState("");
+
+  const handleSubmit = () => {
+    handleAddTask(task);
+    setTask("");
+  };
   return (
-    <div className={styles.todoForm}>
-      <input data-testid="add-task-input" type="text" />
-      <button data-testid="add-task-button"></button>
+    <div className={styles.addTask}>
+      <input
+        data-testid="add-task-input"
+        type="text"
+        value={task}
+        placeholder="Add task..."
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button data-testid="add-task-button" onClick={handleSubmit}>
+        +
+      </button>
     </div>
   );
 };
